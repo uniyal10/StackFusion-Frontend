83231bdf9d3285946e90780960a0007309552335
uniyal10 <uniyal1001@gmail.com>
2023-06-01 15:55:58 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React, { useState } from "react"
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom"
+import HomePage from "./HomePage"
+
+const FormComponent = () => {
+  const [name, set_name] = useState("")
+  const [email, set_email] = useState("")
+  const [dob, set_dob] = useState()
+  const [phone_number, set_phone_number] = useState()
+  const navigate = useNavigate()
+
+  async function handleSubmit() {
+    let myHeaders = new Headers()
+    myHeaders.append("Content-Type", "application/json")
+
+    let raw = JSON.stringify({
+      name: name,
+      email: email,
+      phone_number: phone_number,
+      dob: dob
+    })
+
+    let headerOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow"
+    }
+
+    const res = await fetch(process.env.REACT_APP_URL + "/api/create-user", headerOptions)
+    if (res.ok) {
+      navigate("/home")
+    }
+  }
+  return (
+    <div className="container w-1/4 mx-auto py-8 shadow-lg p-10 ">
+      <h1 className="text-2xl font-bold mb-6 text-center">Add User</h1>
+
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" HtmlFor="name">
+          Name
+        </label>
+        <input onChange={e => set_name(e.target.value)} className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="text" id="name" name="name" placeholder="John Doe" />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" HtmlFor="email">
+          Email
+        </label>
+        <input onChange={e => set_email(e.target.value)} className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="email" id="email" name="email" placeholder="john@example.com" />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" HtmlFor="phone_number">
+          Phone Number
+        </label>
+        <input onChange={e => set_phone_number(e.target.value)} className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="text" id="phone_number" name="phone_number" placeholder="Enter Phone Number" />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" HtmlFor="date">
+          Date of Birth
+        </label>
+        <input onChange={e => set_dob(e.target.value)} className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="date" id="date" name="date" />
+      </div>
+      <button onClick={handleSubmit} className="w-full bg-indigo-500 text-white text-sm font-bold py-2 px-4 rounded-md hover:bg-indigo-600 transition duration-300" type="submit">
+        Add User
+      </button>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<FormComponent />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/HomePage.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react"
+
+const get_all_forms = async () => {
+  let myheaders = new Headers()
+  myheaders.append("Content-Type", "application/json")
+
+  let requestOptions = {
+    method: "GET",
+    headers: myheaders,
+    redirect: "follow"
+  }
+
+  return await (await fetch("http://localhost:8080/api/get-users", requestOptions)).json()
+}
+
+const HomePage = () => {
+  const [all_forms, set_all_forms] = useState([])
+  useEffect(() => {
+    ;(async () => {
+      set_all_forms(await get_all_forms())
+    })()
+  }, [])
+  return (
+    <div className=" grid grid-cols-2 m-2">
+      {all_forms.map(data => {
+        return (
+          <a href="#" class="flex flex-col items-center m-10 p-2 justify-center w-full bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full  h-96 md:h-full md:w-48  md:rounded-lg" src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="" />
+            <div class="flex flex-col justify-between p-4 leading-normal">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{data?.name}</h5>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Email: {data?.email}</p>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Phone: {data?.phone_number}</p>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">DOB: {new Date(data?.dob).toDateString("hi")}</p>
+            </div>
+          </a>
+        )
+      })}
+    </div>
+  )
+}
+
+export default HomePage
