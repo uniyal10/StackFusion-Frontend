f60a017a587e0ab2876c22847a0ac4002c45c91c
SUDHANSHU UNIYAL <uniyal1001@gmail.com>
2023-06-01 19:05:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css"
 import React, { useState } from "react"
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom"
 import HomePage from "./HomePage"
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 const FormComponent = () => {
   const [name, set_name] = useState("")
@@ -31,6 +33,8 @@ const FormComponent = () => {
     const res = await fetch(process.env.REACT_APP_URL + "/api/create-user", headerOptions)
     if (res.ok) {
       navigate("/home")
+    } else {
+      toast((await res.json()).message)
     }
   }
   return (
@@ -59,7 +63,7 @@ const FormComponent = () => {
         <label className="block text-gray-700 text-sm font-bold mb-2" HtmlFor="date">
           Date of Birth
         </label>
-        <input onChange={e => set_dob(e.target.value)} className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="date" id="date" name="date" />
+        <input onChange={e => set_dob(e.target.value)} max={`${new Date().getFullYear()}-${new Date().getMonth() > 9 ? new Date().getMonth() + 1 : "0" + (new Date().getMonth() + 1)}-${new Date().getDate() > 9 ? new Date().getDate() + 1 : "0" + new Date().getDate()}`} className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="date" id="date" name="date" />
       </div>
       <button onClick={handleSubmit} className="w-full bg-indigo-500 text-white text-sm font-bold py-2 px-4 rounded-md hover:bg-indigo-600 transition duration-300" type="submit">
         Add User
@@ -70,12 +74,15 @@ const FormComponent = () => {
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<FormComponent />} />
-        <Route path="/home" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<FormComponent />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer position="top-center" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light" />
+    </>
   )
 }
 
